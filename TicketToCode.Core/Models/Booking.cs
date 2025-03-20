4f711f6dc2b71002d602d3f49c54b835a336276c
AiWictoria <viktoria.pakhrova@outlook.com>
2025-03-20 19:02:30 +0100

    Added model for booking in core project. Next to do: Add enpoints in api and db

---
+++ TicketToCode.Core/Models/Booking.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketToCode.Core.Models;
+
+public class Booking
+{
+    public int BookingId { get; set; }
+    public int EventId { get; set; }
+    [Required]
+    public string FirstName { get; set; } = string.Empty;
+    [Required]
+    public string LastName { get; set; } = string.Empty;
+    [Required]
+    [Phone]
+    public string Phone { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Range(1, int.MaxValue, ErrorMessage = "Select at least one ticket to continue")]
+    public int Tickets { get; set; } = 1;
+    [Required]
+    public PaymentMethod PaymentMethod { get; set; }
+}
+public enum PaymentMethod
+{
+    CreditCard,
+    Swish,
+    Invoice
+}
