b94a1b6f0bb92f821536b1a5cec8fe65762a2794
AiWictoria <viktoria.pakhrova@outlook.com>
2025-03-24 19:11:54 +0100

    Added CustomAuthenticationStateProvider for localStorage-based auth handling like hiding admin pages.

--- BlazorTicketCode/Program.cs
+++ BlazorTicketCode/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorTicketCode;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,5 +10,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7206/") });
 builder.Services.AddScoped<AuthStateService>();
 builder.Services.AddScoped<BookingState>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddAuthorizationCore();
 
 await builder.Build().RunAsync();
